51d879eb276e699bd1c30584b190236263f67036
Lucas Maciel <lucasmaciel63@gmail.com>
2022-04-03 21:59:16 -0300

    up

---
+++ reactapp/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component{
+  render(){
+    var tasks = this.state.todoList
+    var self = this
+    return(
+        <div className="container">
+
+          <div id="task-container">
+              <div  id="form-wrapper">
+                 <form onSubmit={this.handleSubmit}  id="form">
+                    <div className="flex-wrapper">
+                        <div style={{flex: 6}}>
+                            <input onChange={this.handleChange} className="form-control" id="title" value={this.state.activeItem.title} type="text" name="title" placeholder="Add task.." />
+                         </div>
+
+                         <div style={{flex: 1}}>
+                            <input id="submit" className="btn btn-warning" type="submit" name="Add" />
+                          </div>
+                      </div>
+                </form>
+             
+              </div>
+
+              <div  id="list-wrapper">         
+                    
+              </div>
+          </div>
+          
+        </div>
+      )
+  }
+}
+
+export default App;
